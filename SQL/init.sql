391471851cb886ac603ddc95d733f2a062ad8fa3
AlGaRitm <albert.gareev.2018@yandex.ru>
2025-06-28 14:50:23 +1000

    added docker

--- SQL/init.sql
+++ SQL/init.sql
@@ -7,3 +7,5 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+-- user: test, password: 123
+INSERT INTO users (username, password) VALUES ('test', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9LLC6xZ0f7FKQE6TcG5PIHZGOm');
